4c233b9e16fd8700301433c9e990980e3d93f8b2
Juan José Jiménez Gil <juanjojimgil@hotmail.com>
2026-06-10 23:17:18 +0200

    Annotate MainActivity with @AndroidEntryPoint

--- app/src/main/java/com/juanjojmnz/gtavguide/MainActivity.kt
+++ app/src/main/java/com/juanjojmnz/gtavguide/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.navigation.compose.rememberNavController
 import com.juanjojmnz.gtavguide.navigation.GTANavGraph
 import com.juanjojmnz.gtavguide.ui.theme.GTAVGuideTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
