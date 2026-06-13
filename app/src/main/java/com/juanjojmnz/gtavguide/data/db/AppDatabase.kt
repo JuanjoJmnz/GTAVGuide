aab3f7c2ac8ea8d832b85a1d03ab915f5189bd1a
Juan José Jiménez Gil <juanjojimgil@hotmail.com>
2026-06-13 16:56:56 +0200

    Add PropertyDao and RandomEventDao

--- app/src/main/java/com/juanjojmnz/gtavguide/data/db/AppDatabase.kt
+++ app/src/main/java/com/juanjojmnz/gtavguide/data/db/AppDatabase.kt
@@ -8,19 +8,25 @@ import androidx.room.TypeConverters
 import androidx.sqlite.db.SupportSQLiteDatabase
 import com.juanjojmnz.gtavguide.model.Converters
 import com.juanjojmnz.gtavguide.model.Mission
+import com.juanjojmnz.gtavguide.model.Property
+import com.juanjojmnz.gtavguide.model.RandomEvent
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 @Database(
-    entities = [Mission::class],
-    version = 2,
+    entities = [Mission::class, Property::class, RandomEvent::class],
+    version = 4,
     exportSchema = false
 )
+
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun missionDao(): MissionDao
+    abstract fun propertyDao(): PropertyDao
+
+    abstract fun randomEventDao(): RandomEventDao
 
     companion object {
         @Volatile private var INSTANCE: AppDatabase? = null
@@ -35,7 +41,6 @@ abstract class AppDatabase : RoomDatabase() {
                     .addCallback(object : Callback() {
                         override fun onCreate(db: SupportSQLiteDatabase) {
                             super.onCreate(db)
-                            // Poblar la BD la primera vez desde JSON
                             INSTANCE?.let { database ->
                                 CoroutineScope(Dispatchers.IO).launch {
                                     DatabaseSeeder(context).seed(database)
@@ -43,7 +48,7 @@ abstract class AppDatabase : RoomDatabase() {
                             }
                         }
                     })
-                    .fallbackToDestructiveMigration()
+                    .fallbackToDestructiveMigration(false)
                     .build()
                     .also { INSTANCE = it }
             }
