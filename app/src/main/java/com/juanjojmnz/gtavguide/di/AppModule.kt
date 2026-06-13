5cfc05661329e68bc041921594a91b20b6e32a7d
Juan José Jiménez Gil <juanjojimgil@hotmail.com>
2026-06-13 17:00:59 +0200

    Add Property and RandomEvent providers to AppModule

--- app/src/main/java/com/juanjojmnz/gtavguide/di/AppModule.kt
+++ app/src/main/java/com/juanjojmnz/gtavguide/di/AppModule.kt
@@ -3,7 +3,11 @@ package com.juanjojmnz.gtavguide.di
 import android.content.Context
 import com.juanjojmnz.gtavguide.data.db.AppDatabase
 import com.juanjojmnz.gtavguide.data.db.MissionDao
+import com.juanjojmnz.gtavguide.data.db.PropertyDao
+import com.juanjojmnz.gtavguide.data.db.RandomEventDao
 import com.juanjojmnz.gtavguide.data.repository.MissionRepository
+import com.juanjojmnz.gtavguide.data.repository.PropertyRepository
+import com.juanjojmnz.gtavguide.data.repository.RandomEventRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -30,4 +34,24 @@ object AppModule {
     @Singleton
     fun provideMissionRepository(dao: MissionDao): MissionRepository =
         MissionRepository(dao)
+
+    @Provides
+    @Singleton
+    fun providePropertyDao(db: AppDatabase): PropertyDao =
+        db.propertyDao()
+
+    @Provides
+    @Singleton
+    fun providePropertyRepository(dao: PropertyDao): PropertyRepository =
+        PropertyRepository(dao)
+
+    @Provides
+    @Singleton
+    fun provideRandomEventDao(db: AppDatabase): RandomEventDao =
+        db.randomEventDao()
+
+    @Provides
+    @Singleton
+    fun provideRandomEventRepository(dao: RandomEventDao): RandomEventRepository =
+        RandomEventRepository(dao)
 }
